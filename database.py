c24c9b20ea59535e9c28e1ec2882f0ceaca805b6
MaksMakovskiy <dota.money228@gmail.com>
2022-07-19 15:32:48 +0300

    main

--- database.py
+++ database.py
@@ -7,15 +7,6 @@ class DataCrude():
             "base.db", check_same_thread=False)
         self.fether = self.sqlconn.cursor()
         self.players = {}
-        self.DoDIctUsers()
-
-
-    def message_chek(self, id):
-        self.DoDIctUsers
-        if id in self.players:
-            return True
-        else:
-            return False
 
 
     def PrintUserToBase(self, id, prim=0):
@@ -23,22 +14,6 @@ class DataCrude():
             self.fether.execute(
                 f"INSERT INTO player (id, primogem) VALUES ('{id}', '{prim}');")
             self.sqlconn.commit()
-
-
-    def newplayerfuch(self, id):
-        if self.message_chek(id) == True:
-            return True
-        else:
-            self.PrintUserToBase(id)
-            return False
-
-
-    def DoDIctUsers(self):
-        self.fether.execute("SELECT * FROM player;")
-        record = self.fether.fetchall()
-
-        for i in range(len(record)):
-            self.players[record[i][0]] = {"primogem": record[i][1], "lastuse": record[i][2]}
 
 
     def ConnClose(self):
--- main.py
+++ main.py
@@ -10,67 +10,41 @@ bot = Bot(token='5584354396:AAFs6RqDC3_RfPjeHbUzU6qGIGVVE2qD1n8')
 dp = Dispatcher(bot)
 
 
-commands = ("/primogem","/newplayer","/roll","/flip","/war")
-
-
-def warcommand(name):
-    if random.randint(0, 1) == 0:
-        return f"{name} выйграл эту битву!"
-    else:
-        return f"{name} потерпел поражение!"
+commands = ("/create_clan", "/delete_clan", "/role")
 
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm GSHbot!\nfor all command use /gshlist")
+    await message.reply("Hi!\nI'm GSHbot!\nbot for ur team\nfor all command use /gshlist")
 
 
-@dp.message_handler(commands=['flip'])
-async def get_message(message: types.Message):
-    if random.randint(0, 1) == 0:
-        await message.reply("Орёл")
+@dp.message_handler(commands=['create_clan'])
+async def send_welcome(message: types.Message):
+    if len(message.text.split()) == 2:
+        clan_name = message.text.split()[1]
+        await message.reply(f"✅clan created whits name '{clan_name}'✅")
     else:
-        await message.reply("Решка")
+        await message.reply("Please use '/create_clan [clan_name]'\nClan name can be only 1 word")
 
 
-@dp.message_handler(commands=['roll'])
-async def get_message(message: types.Message):
-    await message.reply(f"{message.from_user.first_name}: {random.randint(1, 100)}|100")
+@dp.message_handler(commands=['role'])
+async def send_welcome(message: types.Message):
+    await message.reply("clan created")
+
+
+@dp.message_handler(commands=['delete_clan'])
+async def send_welcome(message: types.Message):
+    if len(message.text.split()) == 2:
+        clan_name = message.text.split()[1]
+        await message.reply(f"✅clan whits name '{clan_name}' was deleted✅")
+    else:
+        await message.reply("Please use '/delete_clan [clan_name]'\nClan name can be only 1 word")
 
 
 @dp.message_handler(commands=['gshlist'])
 async def get_message(message: types.Message):
     await message.reply("\n".join(commands))
 
-
-@dp.message_handler(commands=['primogem'])
-async def get_message(ms: types.Message):
-    if b.message_chek(ms.from_user.id) == True:
-        await bot.send_message(chat_id=ms.chat.id,
-          text=( "У " + ms.from_user.first_name + " " + str(b.players[ms.from_user.id]["primogem"]) + " встреч"))
-    elif (ms.text).startswith(commands) == True:
-            await bot.send_message(chat_id=ms.chat.id,
-            text=("Вас нету в списках игроков! Хотите зарегестрироваться? /newplayer"))
-    
-
-@dp.message_handler(commands=['war'])
-async def get_message(ms: types.Message):
-    if b.message_chek(ms.from_user.id) == True:        
-        await bot.send_message(chat_id=ms.chat.id,
-            text=(warcommand(ms.from_user.first_name))) 
-    elif (ms.text).startswith(commands) == True:
-            await bot.send_message(chat_id=ms.chat.id,
-            text=("Вас нету в списках игроков! Хотите зарегестрироваться? /newplayer"))
-
-
-@dp.message_handler(commands=['newplayer'])
-async def get_message(ms: types.Message):
-    if b.message_chek(ms.from_user.id) == True:
-        await ms.reply("Вы уже зарегистрированный игрок!")                
-    else:
-        if b.newplayerfuch(ms.from_user.id) != True:
-            await ms.reply("Вы зарегестрированны!")
-        
 
 @dp.message_handler()
 async def get_message(ms):
