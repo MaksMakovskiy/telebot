9835efd800f5939725f3edf60f381d3bbc80e102
MaksMakovskiy <dota.money228@gmail.com>
2022-07-20 15:40:04 +0300

    main

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+from doctest import FAIL_FAST
 import sqlite3
 
 
@@ -6,14 +7,63 @@ class DataCrude():
         self.sqlconn = sqlite3.connect(
             "base.db", check_same_thread=False)
         self.fether = self.sqlconn.cursor()
-        self.players = {}
 
 
-    def PrintUserToBase(self, id, prim=0):
-        if id not in self.players:
+    def CheakClanName(self, ClanName) -> True|False:
+        self.fether.execute(
+            f"SELECT Name FROM Clans WHERE name='{ClanName}'"
+        )
+        ClanInfo = self.fether.fetchall()
+        if ClanInfo != []:    
+            if ClanName == ClanInfo[0][0]:
+                return True         
+            else:
+                return False
+        else:
+            return False
+
+
+    def CheakClanLider(self, ClanName) -> True|False:
+        if self.CheakClanName(ClanName) == True: 
             self.fether.execute(
-                f"INSERT INTO player (id, primogem) VALUES ('{id}', '{prim}');")
+                f"SELECT Lider FROM Clans WHERE name='{ClanName}'"
+            )
+            return self.fether.fetchall()[0][0]    
+
+    def CreateClan(self, ClanName, LiderName) -> True|False: 
+        if self.CheakClanName(ClanName) == False:
+            self.fether.execute(
+                    f"INSERT INTO Clans (Name, Lider) VALUES ('{ClanName}', '{LiderName}');")
             self.sqlconn.commit()
+            self.fether.execute(f'''
+                CREATE TABLE "{ClanName}" (
+	            Id	INTEGER NOT NULL UNIQUE,
+	            Name	TEXT NOT NULL UNIQUE,
+	            PRIMARY KEY(Id)
+            )''')
+            self.sqlconn.commit()
+            self.fether.execute(
+                    f"INSERT INTO {ClanName} (Name) VALUES ('{LiderName}');")
+            self.sqlconn.commit()    
+            return True
+        else:
+            return False
+
+
+    def DeleteClan(self, ClanName, LiderName) -> True|False:
+        if self.CheakClanName(ClanName) == True:
+            if LiderName == self.CheakClanLider(ClanName):
+                self.fether.execute(
+                        f"DELETE FROM Clans WHERE Name='{ClanName}';")
+                self.sqlconn.commit()
+                self.fether.execute(
+                        f"DROP TABLE {ClanName};")
+                self.sqlconn.commit()  
+                return True  
+            else:
+                return False
+        else:
+            return False
 
 
     def ConnClose(self):
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import random
 
 
 b = DataCrude()
-b.DoDIctUsers()
 bot = Bot(token='5584354396:AAFs6RqDC3_RfPjeHbUzU6qGIGVVE2qD1n8')
 dp = Dispatcher(bot)
 
@@ -22,9 +21,26 @@ async def send_welcome(message: types.Message):
 async def send_welcome(message: types.Message):
     if len(message.text.split()) == 2:
         clan_name = message.text.split()[1]
-        await message.reply(f"✅clan created whits name '{clan_name}'✅")
+        ifo = b.CreateClan(clan_name, message.from_user.username)
+        if ifo == True:
+            await message.reply(f"✅clan created whits name '{clan_name}'✅")
+        else:
+            await message.reply("This clan name is allredy used")
     else:
         await message.reply("Please use '/create_clan [clan_name]'\nClan name can be only 1 word")
+
+
+@dp.message_handler(commands=['claim'])
+async def send_welcome(message: types.Message):
+    if len(message.text.split()) == 3:
+        UserName = message.text.split()[1]
+        ClanName = message.text.split()[2]
+        if UserName.startswith("@") == False:
+            await message.reply("False")
+        else:
+            await message.reply(f"✅OK✅")
+    else:
+        await message.reply("False")
 
 
 @dp.message_handler(commands=['role'])
@@ -36,7 +52,11 @@ async def send_welcome(message: types.Message):
 async def send_welcome(message: types.Message):
     if len(message.text.split()) == 2:
         clan_name = message.text.split()[1]
-        await message.reply(f"✅clan whits name '{clan_name}' was deleted✅")
+        ifo = b.DeleteClan(clan_name, message.from_user.username)
+        if ifo == True:
+            await message.reply(f"✅clan whits name '{clan_name}' was deleted✅")
+        else:
+            await message.reply("False")
     else:
         await message.reply("Please use '/delete_clan [clan_name]'\nClan name can be only 1 word")
 
@@ -48,6 +68,7 @@ async def get_message(message: types.Message):
 
 @dp.message_handler()
 async def get_message(ms):
+    print(ms)
     print("@" + ms.from_user.username + ": " + ms.text)
 
 
